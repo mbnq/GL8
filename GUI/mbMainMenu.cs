fc371bc78c331212a2422dc8321329d9746eea5a
mbnq <mbnq00@gmail.com>
2024-10-17 11:33:47 +0200

    Add project files.

---
+++ GUI/mbMainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace GL8.GUI
+{
+    public partial class mbMainMenu : MaterialForm
+    {
+        public mbMainMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
